f3c5e30e2111ae2a31c23da3099f3a3f67702c42
RameshXt <rameshxt.1@gmail.com>
2025-04-28 15:37:31 +0530

    Refactor: minor structure changes

--- config/db.php
+++ config/db.php
@@ -2,13 +2,13 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'pgsql:host=yii2db.catigkm00gyz.us-east-1.rds.amazonaws.com;port=5432;dbname=your_database_name',
     'username' => 'yii2db',
-    'password' => '*28',
+    'password' => 'yii2db*28',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 60,
+    'schemaCache' => 'cache',
 ];
