caca48bc775c04365fbcfa702977f27d353140ba
RameshXt <rameshxt.1@gmail.com>
2025-04-29 16:47:56 +0530

    Fix: correct target file

--- image-updater.sh
+++ image-updater.sh
@@ -4,6 +4,6 @@ IMAGE_TAG=$1
 
 # IMAGE UPDATER
 echo "Updating ecs.tf with the new image tag: $IMAGE_TAG"
-sed -i "s|image\s*=\s*\".*\"|image = \"$IMAGE_TAG\"|" docker-compose.yml
+sed -i "s|image\s*=\s*\".*\"|image = \"$IMAGE_TAG\"|" Docker-stack.yml
 
 echo "ecs.tf file updated successfully with image tag: $IMAGE_TAG"
