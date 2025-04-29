bcb7c522ccb765b69757aea69f571bc7f60783ee
RameshXt <rameshxt.1@gmail.com>
2025-04-29 16:51:22 +0530

    Refactor: Step: 6 IMAGE UPDATER ON Docker-stack.yml

--- image-updater.sh
+++ image-updater.sh
@@ -3,7 +3,7 @@
 IMAGE_TAG=$1
 
 # IMAGE UPDATER
-echo "Updating ecs.tf with the new image tag: $IMAGE_TAG"
-sed -i "s|image\s*=\s*\".*\"|image = \"$IMAGE_TAG\"|" Docker-stack.yml
+echo "Updating Docker-stack.yml with the new image tag: $IMAGE_TAG"
+sed -i "s|image\s*=\s*\"[^\"]*\"|image = \"$IMAGE_TAG\"|" Docker-stack.yml
 
-echo "ecs.tf file updated successfully with image tag: $IMAGE_TAG"
+echo "Docker-stack.yml file updated successfully with image tag: $IMAGE_TAG"
