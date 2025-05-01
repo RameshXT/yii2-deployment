5db36d7de9277a02a7a1fd745af8622247ab919e
RameshXT <rameshxt.1@gmail.com>
2025-05-02 00:08:20 +0530

    chore(config): remove Gii and Debug modules for production deployment

--- config/web.php
+++ config/web.php
@@ -6,7 +6,7 @@ $db = require __DIR__ . '/db.php';
 $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
+    'bootstrap' => ['log'], // no 'gii' or 'debug'
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
@@ -54,21 +54,6 @@ $config = [
     'params' => $params,
 ];
 
-// if (YII_ENV_DEV) {
-//     // configuration adjustments for 'dev' environment
-//     $config['bootstrap'][] = 'debug';
-//     $config['modules']['debug'] = [
-//         'class' => 'yii\debug\Module',
-//         // uncomment the following to add your IP if you are not connecting from localhost.
-//         'allowedIPs' => ['127.0.0.1', '::1', '3.238.93.65'],
-//     ];
-
-//     $config['bootstrap'][] = 'gii';
-//     $config['modules']['gii'] = [
-//         'class' => 'yii\gii\Module',
-//         // uncomment the following to add your IP if you are not connecting from localhost.
-//         'allowedIPs' => ['127.0.0.1', '::1', '3.238.93.65'],
-//     ];
-// }
+// Development tools like 'gii' and 'debug' are intentionally omitted for production.
 
 return $config;
